83af841286658eabd737d2dd04771e12da165d0c
siddharth anand <sidcode973@gmail.com>
2026-03-06 02:09:58 +0530

    finalize app

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,18 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
-import {events} from "@/lib/constants"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache';
-     cacheLife('hours')
-    //const response = await fetch(`${BASE_URL}/api/events`);
-    //const { events } = await response.json();
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
     return (
         <section>
-            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&apos;t  Miss</h1>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&apos;t Miss</h1>
             <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
             <ExploreBtn />
